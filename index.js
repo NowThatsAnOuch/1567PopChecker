374da2a459d818cbec1641f6bbbe07c93588f2ab
NowThatsAnOuch <armykid79@gmail.com>
2023-03-28 20:46:33 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,64 +1,37 @@
-const express = require('express');
+const express = import('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const functions = import('./node.js');
 
-const {
-  getNumPlayers,
-  getMessage,
-  sendMessage,
-  editMessage,
-  startEditingMessage,
-  stopEditingMessage,
-  initialize,
-  setWebhookURL,
-} = require('./functions');
-
-app.get('/getNumPlayers', async (req, res) => {
-  const serverName = req.query.serverName;
-  const numPlayers = await getNumPlayers(serverName);
-  res.send({ numPlayers });
-});
-
-app.get('/getMessage', async (req, res) => {
-  const message = await getMessage();
-  res.send({ message });
-});
-
-app.post('/sendMessage', async (req, res) => {
-  const content = req.body.content;
-  const response = await sendMessage(content);
-  res.send(response);
-});
-
-app.patch('/editMessage', async (req, res) => {
-  const messageID = req.body.messageID;
-  const content = req.body.content;
-  const response = await editMessage(messageID, content);
-  res.send(response);
-});
-
-app.post('/startEditingMessage', async (req, res) => {
-  const messageID = req.body.messageID;
-  await startEditingMessage(messageID);
-  res.send({ message: 'Editing started' });
-});
-
-app.post('/stopEditingMessage', async (req, res) => {
-  await stopEditingMessage();
-  res.send({ message: 'Editing stopped' });
-});
-
-app.get('/initialize', async (req, res) => {
-  const response = await initialize();
-  res.send(response);
-});
-
-app.post('/setWebhookURL', async (req, res) => {
-  const webhookURL = req.body.webhookURL;
-  await setWebhookURL(webhookURL);
-  res.send({ message: 'Webhook URL set' });
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
+});
+
+// Now you can use the exported functions in your server routes
+app.get('/start', (req, res) => {
+  functions.startEditingMessage();
+  res.send('Started editing message');
+});
+
+app.get('/stop', (req, res) => {
+  functions.stopEditingMessage();
+  res.send('Stopped editing message');
+});
+
+app.get('/message', async (req, res) => {
+  await functions.getMessage();
+  res.send('Fetched message');
+});
+
+app.get('/players', (req, res) => {
+  const serverPop = functions.getNumPlayers("EU-PVP-XboxOfficial-GenTwo1567", functions.servers);
+  if (serverPop !== null) {
+    res.send(`Server population on **1567** is: **${serverPop}**`);
+  } else {
+    res.send('Server not found');
+  }
 });
